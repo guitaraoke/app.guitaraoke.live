e83946dd1f6352ee0d6bd2ee3351456184660432
Dylan Beattie <dylan@dylanbeattie.net>
2023-07-12 13:34:04 +0100

    Snapshot before installing AngleSharp

--- GuitaraokeWebApp.Tests/WebTests.cs
+++ GuitaraokeWebApp.Tests/WebTests.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 
 namespace GuitaraokeWebApp.Tests;
@@ -19,6 +20,26 @@ public class WebTests : IClassFixture<WebApplicationFactory<Program>> {
 		var html = await response.Content.ReadAsStringAsync();
 		var decodedHtml = WebUtility.HtmlDecode(html);
 		var db = factory.Services.GetService<ISongDatabase>();
+		foreach (var song in db.ListSongs()) {
+			decodedHtml.ShouldContain(song.Title);
+			decodedHtml.ShouldContain(song.Artist);
+		}
+	}
+
+	[Fact]
+	public async Task Root_Index_Includes_Starred_Songs() {
+		var userGuid = Guid.NewGuid();
+		var client = factory.CreateClient();
+		var db = factory.Services.GetService<ISongDatabase>();
+		db!.ToggleStar(userGuid, db.ListSongs().First());
+		var request = new HttpRequestMessage(HttpMethod.Get, "/");
+		var cookie = new Cookie(HttpCookieUserTracker.COOKIE_NAME,
+			userGuid.ToString());
+		request.Headers.Add("Cookie", cookie.ToString());
+		var response = await client.SendAsync(request);
+		var html = await response.Content.ReadAsStringAsync();
+		var decodedHtml = WebUtility.HtmlDecode(html);
+
 		foreach (var song in db.ListSongs()) {
 			decodedHtml.ShouldContain(song.Title);
 			decodedHtml.ShouldContain(song.Artist);
