5ca624adc9ac783a2a0714d0f5c7827340ab86c5
smabuk <2011834+smabuk@users.noreply.github.com>
2023-07-22 03:23:44 +0100

    no need to add controllers at startup

--- GuitaraokeBlazorApp/Program.cs
+++ GuitaraokeBlazorApp/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
 	.AddServerComponents();
 
-builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 
 builder.Services.AddHttpContextAccessor();
