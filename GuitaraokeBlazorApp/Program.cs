27422fd45c62b90539331ce5f54cd1b15c3f9790
smabuk <2011834+smabuk@users.noreply.github.com>
2023-10-12 15:45:19 +0100

    .NET 8.0 rc2

--- GuitaraokeBlazorApp/Program.cs
+++ GuitaraokeBlazorApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor()
-	.AddServerComponents();
+	.AddInteractiveServerComponents();
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ICookieJar, HttpCookieJar>();
@@ -22,11 +22,12 @@ builder.Services.AddSingleton<ISongDatabase>(db);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+if (app.Environment.IsDevelopment()) {
+	//app.UseWebAssemblyDebugging();
+} else {
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -35,8 +36,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAntiforgery();
+
 app.MapRazorComponents<App>()
-	.AddServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 app.MapSongEndpoints();
 app.MapBackstageEndpoints();
